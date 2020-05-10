f0e41d8bef1cc052bebb07b33242e9cab6f13f3b
mtthoag <moagazaly@yahoo.com>
2020-05-10 16:24:14 -0400

    Add files via upload
    added labels array to make game run faster, added board to bots to make the comparisons better

--- bot_brigade.py
+++ bot_brigade.py
@@ -1,5 +1,7 @@
 from tkinter import *
+import tkinter.font as font
 from bots import *
+import random
 
 """
 0 is the selected dropping cell
@@ -12,8 +14,13 @@ from bots import *
 7 is selected scissor   
 8 is a blank square
 """
-global finish
-finish = True
+global master
+master = Tk()
+
+global p1s
+p1s = 0
+global p2s
+p2s=0
 
 global p1_rps 
 p1_rps = 5
@@ -28,11 +35,62 @@ global p2_rps
 p2_rps = 5
 global p2_bots
 p2_bots = []
+global r_mode 
+r_mode = False
+
 
 global board
 board = [[1 for n in range(8)] for i in range(8)]
-master = Tk()
+
 img = [ PhotoImage(file=str(i)+ ".png").subsample(2,2) for i in range(9)]
+for i in range(9,12):
+    img.append(0)
+for i in range(12,15):
+    img.append(PhotoImage(file=str(i)+ ".png").subsample(2,2))
+
+#Making the board
+labels = [[Label(master, image = img[1]) for c in range(8)] for r in range(10)]
+
+for c in range(8):
+    r = 0
+    labels[r][c].destroy()
+    r = 9
+    labels[r][c].destroy()
+for r in range(1,8):
+    for c in range(8):
+        labels[r][c].grid(row = r, column = c)
+for r in range(1,8):
+    c = 0
+    labels[r][c]['image'] = img[8]
+    c = 7
+    labels[r][c]['image'] = img[8]
+        
+
+labels[4][0]['image'] = img[0]
+labels[4][7]['image'] = img[0]
+labels[9][0] = Label(master, image = img[5])
+labels[9][0].grid(row = 9, column = 0, pady=10)
+
+labels[9][5] = Label(master, image = img[5])
+labels[9][5].grid(row = 9, column = 5, pady=10)
+
+labels[9][1] = Label(master, image = img[3])
+labels[9][1].grid(row = 9, column = 1, pady=10)
+
+labels[9][6] = Label(master, image = img[3])
+labels[9][6].grid(row = 9, column = 6, pady=10)
+
+labels[9][2] = Label(master, image = img[4])
+labels[9][2].grid(row = 9, column = 2, pady=10)
+
+labels[9][7] = Label(master, image = img[4])
+labels[9][7].grid(row = 9, column = 7, pady=10)
+
+labels[9][3] = Label(master, text=p1s)
+labels[9][3].grid(row=9, column=3, pady=10)
+labels[9][4] = Label(master, text=p2s)
+labels[9][4].grid(row=9, column=4, pady=10)
+
 
 #player 1 movement
 def rps_a(event):
@@ -41,15 +99,14 @@ def rps_a(event):
     x = (x-1) % 3
     #switch betweent r, p, s selection first line highlights the new pic, second line unhighlights the old one
     if x == 0:
-        Label(master, image = img[5]).grid(row = 9, column = x, pady=10)
-        Label(master, image = img[3]).grid(row = 9, column = x+1, pady=10)
+        labels[9][x]['image'] = img[5]
+        labels[9][x+1]['image'] = img[3]
     elif x == 1:
-        Label(master, image = img[6]).grid(row = 9, column = x, pady=10)
-        Label(master, image = img[4]).grid(row = 9, column = x+1, pady=10)
+        labels[9][x]['image'] = img[6]
+        labels[9][x+1]['image'] = img[4]
     else:
-        Label(master, image = img[7]).grid(row = 9, column = x, pady=10)
-        Label(master, image = img[2]).grid(row = 9, column = x-2, pady=10)
-
+        labels[9][x]['image'] = img[7]
+        labels[9][x-2]['image'] = img[2]
     p1_rps = x+5
 
 def rps_d(event):
@@ -58,15 +115,14 @@ def rps_d(event):
     x = (x+1) % 3
     #switch betweent r, p, s selection first line highlights the new pic, second line unhighlights the old one
     if x == 0:
-        Label(master, image = img[5]).grid(row = 9, column = x, pady=10)
-        Label(master, image = img[4]).grid(row = 9, column = x+2, pady=10)
+        labels[9][x]['image'] = img[5]
+        labels[9][x+2]['image'] = img[4]
     elif x == 1:
-        Label(master, image = img[6]).grid(row = 9, column = x, pady=10)
-        Label(master, image = img[2]).grid(row = 9, column = x-1, pady=10)
+        labels[9][x]['image'] = img[6]
+        labels[9][x-1]['image'] = img[2]
     else:
-        Label(master, image = img[7]).grid(row = 9, column = x, pady=10)
-        Label(master, image = img[3]).grid(row = 9, column = x-1, pady=10)
-
+        labels[9][x]['image'] = img[7]
+        labels[9][x-1]['image'] = img[3]
     p1_rps = x+5
 
 def row_w(event):
@@ -74,8 +130,8 @@ def row_w(event):
     x = (p1_row - 1) % 8
     if x == 0:
         x =7
-    Label(master, image = img[0]).grid(row = x, column = 0)
-    Label(master, image = img[8]).grid(row = p1_row, column = 0)
+    labels[x][0]['image'] = img[0]
+    labels[p1_row][0]['image'] = img[8]
     p1_row = x
 
 def row_s(event):
@@ -83,8 +139,8 @@ def row_s(event):
     x = (p1_row + 1) % 8
     if x == 0:
         x=1
-    Label(master, image = img[0]).grid(row = x, column = 0)
-    Label(master, image = img[8]).grid(row = p1_row, column = 0)
+    labels[x][0]['image'] = img[0]
+    labels[p1_row][0]['image'] = img[8]
     p1_row = x
 
 def rps_t(event):
@@ -95,10 +151,11 @@ def rps_t(event):
     if board[p1_row][1] > 1:
         return
     rps = p1_rps-3
-    b = Bot(rps, p1_row, 1)
+    b = Bot(rps, p1_row, 1, board)
     board[p1_row][1] = rps
     p1_bots.append(b)
-    Label(master, image = img[rps]).grid(row =p1_row, column = 1)
+    labels[p1_row][1]['image'] = img[rps]
+
 
 #player 2 movement
 def rps_left(event):
@@ -107,14 +164,14 @@ def rps_left(event):
     x = (x-1) % 3
     #switch betweent r, p, s selection first line highlights the new pic, second line unhighlights the old one
     if x == 0:
-        Label(master, image = img[5]).grid(row = 9, column = x+5, pady=10)
-        Label(master, image = img[3]).grid(row = 9, column = x+5+1, pady=10)
+        labels[9][x+5]['image'] = img[5]
+        labels[9][x+5+1]['image'] = img[3]
     elif x == 1:
-        Label(master, image = img[6]).grid(row = 9, column = x+5, pady=10)
-        Label(master, image = img[4]).grid(row = 9, column = x+5+1, pady=10)
+        labels[9][x+5]['image'] = img[6]
+        labels[9][x+5+1]['image'] = img[4]
     else:
-        Label(master, image = img[7]).grid(row = 9, column = x+5, pady=10)
-        Label(master, image = img[2]).grid(row = 9, column = x+5-2, pady=10)
+        labels[9][x+5]['image'] = img[7]
+        labels[9][x+5-2]['image'] = img[2]
 
     p2_rps = x+5
 
@@ -124,14 +181,14 @@ def rps_right(event):
     x = (x+1) % 3
     #switch betweent r, p, s selection first line highlights the new pic, second line unhighlights the old one
     if x == 0:
-        Label(master, image = img[5]).grid(row = 9, column = x+5, pady=10)
-        Label(master, image = img[4]).grid(row = 9, column = x+5+2, pady=10)
+        labels[9][x+5]['image'] = img[5]
+        labels[9][x+5+2]['image'] = img[4]
     elif x == 1:
-        Label(master, image = img[6]).grid(row = 9, column = x+5, pady=10)
-        Label(master, image = img[2]).grid(row = 9, column = x+5-1, pady=10)
+        labels[9][x+5]['image'] = img[6]
+        labels[9][x+5-1]['image'] = img[2]
     else:
-        Label(master, image = img[7]).grid(row = 9, column = x+5, pady=10)
-        Label(master, image = img[3]).grid(row = 9, column = x+5-1, pady=10)
+        labels[9][x+5]['image'] = img[7]
+        labels[9][x+5-1]['image'] = img[3]
 
     p2_rps = x+5
 
@@ -140,8 +197,8 @@ def row_up(event):
     x = (p2_row - 1) % 8
     if x == 0:
         x = 7
-    Label(master, image = img[0]).grid(row = x, column = 7)
-    Label(master, image = img[8]).grid(row = p2_row, column = 7)
+    labels[x][7]['image'] = img[0]
+    labels[p2_row][7]['image'] = img[8]
     p2_row = x
 
 def row_down(event):
@@ -149,8 +206,8 @@ def row_down(event):
     x = (p2_row + 1) % 8
     if x == 0:
         x = 1
-    Label(master, image = img[0]).grid(row = x, column = 7)
-    Label(master, image = img[8]).grid(row = p2_row, column = 7)
+    labels[x][7]['image'] = img[0]
+    labels[p2_row][7]['image'] = img[8]
     p2_row = x
 
 def rps_enter(event):
@@ -160,130 +217,181 @@ def rps_enter(event):
     global board
     if board[p2_row][6] > 1:
         return
-    rps = p2_rps-3
-    b = Bot(rps, p2_row, 6)
+    rps = p2_rps-3+10
+    b = Bot(rps, p2_row, 6, board)
     board[p2_row][6] = rps
     p2_bots.append(b)
-    Label(master, image = img[rps]).grid(row =p2_row, column = 6)
-    
-def update():
-    p1_death = []
-    p2_death = []
-    p1w = False
-    p2w = False
-    global finish
+    labels[p2_row][6]['image'] = img[rps] 
 
+myfont = font.Font(size = 8)
+global rand
+rand = Button(master, text="Random")
+rand.grid(row=0, column=4)
+rand['font'] = myfont
+
+global select
+select = Button(master, text="Select")
+
+def random_mode(event):
+    global r_mode
+    r_mode = True
+    rand.grid_forget()
+    select.grid(row=0, column=4)
+def select_mode(event):
+    global r_mode
+    r_mode = False
+    select.grid_forget()
+    rand.grid(row=0, column=4)
+
+rand.bind('<Button-1>', random_mode)
+select.bind('<Button-1>', select_mode)
+
+def play_again():
+    global rematch
+    global message
+    rematch.grid_forget()
+    message.grid_forget()
+    global p2_bots
+    global p1_bots
+    global p1_row
+    global p2_row
+    global p1_rps
+    global p2_rps
+
+    for r in range(1,8):
+        for c in range(1,7):
+            board[r][c] = 1
+            labels[r][c]['image'] = img[1]
+    p1_bots = []
+    p2_bots = []
+    p1_row = 4
+    p2_row = 4
+    p1_rps = 5
+    p2_rps = 5
+    for r in range(1,8):
+        for c in range(8):
+            if c == 0 or c == 7:
+                labels[r][c]['image'] = img[8]
+    labels[4][0]['image'] = img[0]
+    labels[4][7]['image'] = img[0]
+    labels[9][0]['image'] = img[5]
+    labels[9][5]['image'] = img[5]
+    labels[9][1]['image'] = img[3]
+    labels[9][6]['image'] = img[3]
+    labels[9][2]['image'] = img[4]
+    labels[9][7]['image'] = img[4]
+    update()
+
+global rematch
+rematch = Button(master, text="rematch", command=play_again)
+rematch['font'] = myfont
+rematch.grid_forget()
+global message
+message = Label(master, text="")
+message.grid_forget()
+
+
+
+def update():
+    global p1s
+    global p2s
+    global rematch
+    global message
+    global p1_rps
+    global p2_rps
+    finish = True
+    if r_mode:
+        x = random.randint(5,7)    
+        y = random.randint(5,7)    
+        master.unbind('<Left>')
+        master.unbind('<Right>')
+        master.unbind('a')
+        master.unbind('d')
+        labels[9][p1_rps-5]['image'] = img[p1_rps-5+2]
+        labels[9][p2_rps]['image'] = img[p2_rps-5+2]
+        p1_rps = x
+        p2_rps = y
+        labels[9][p1_rps-5]['image'] = img[p1_rps]
+        labels[9][p2_rps]['image'] = img[p2_rps]
+    else:
+        master.bind('<Left>', rps_left)
+        master.bind('<Right>', rps_right)
+        master.bind('a', rps_a)
+        master.bind('d', rps_d)
     for b in p1_bots:
         r = b.r
         c = b.c
-        if c == 6:
-            works = Tk()
-            p1w = True
-            finish = False
-            Label(works, text="Player 1 wins!").grid(row=0)
-            return
-        board[r][c] = 1
-        rps = board[r][c+1]
-        
-        if rps > 1:
-            bb = Bot(rps, r, c+1)
-            x = b.comp(bb)
-            if x == 1:
-                board[r][c+1] = b.rps
-                b.c = c+1
-                p2_death.append(bb)
-            elif x == 0:
-                board[r][c+1] = 1
-                p1_death.append(b)
-                p2_death.append(bb)
+        if b.alive and board[r][c] == b.rps:
+           # print(board, b.rps,'\n') 
+            if c == 6:
+                finish = False
+                message['text'] = "P1 wins!"
+                rematch.grid(row=0, column= 2)
+                message.grid(row=0, column=0)
+                
+                p1s += 1
+                labels[9][3]['text'] = p1s
+                return
+            rps = board[r][c+1]
+            
+            if rps > 1:
+                bb = Bot(rps, r, c+1, board)
+                b.comp(bb)
             else:
-                p1_death.append(b)
-        else:
-            b.c = c+1
-            board[r][c+1] = b.rps
-    
-    for b in p1_death:    
-        p1_bots.remove(b)
-    p1_death = []
-    for b in p2_death:
-        x = 0
-        while x ==0:
-            i = 0
-            if p2_bots[i].r == b.r and p2_bots[i].c == b.c:
-                p2_bots.pop(i)
-                x = 1
-            i += 1
-    p2_death = []
+                b.c = c+1
+                board[r][c] = 1
+                board[r][c+1] = b.rps
+        
+
     for b in p2_bots:
         r = b.r
         c = b.c
-        if c == 1:
-            finish = False
-            works = Tk()
-            Label(works, text="Player 2 wins!").grid(row=0)
-            return
-        board[r][c] = 1
-        rps = board[r][c-1]
-        if rps == 1:
-            board[r][c-1] = b.rps
-            b.c = c-1
-        else:
-            bb = Bot(rps, r, c-1)
-            x = b.comp(bb)
-            if x == 1:
+        if board[r][c] == b.rps and b.alive:
+           # print(board, b.rps, '\n')
+            if c == 1:
+                finish = False   
+                p2s += 1
+                message['text'] = "P2 wins!"
+                rematch.grid(row=0, column= 2)
+                message.grid(row=0, column=0)
+                labels[9][4]['text'] = p2s
+                return
+            
+            rps = board[r][c-1]
+            if rps > 1:
+                bb = Bot(rps, r, c-1, board)
+                b.comp(bb)
+            else:
+                board[r][c] = 1
                 board[r][c-1] = b.rps
                 b.c = c-1
-                p1_death.append(bb)
-            elif x == 0:
-                board[r][c-1] = 1
-                p2_death.append(b)
-                p1_death.append(bb)
-            else:
-                p2_death.append(b)
 
-
-    for b in p2_death:    
-        p2_bots.remove(b)
-    for b in p1_death:
-        x = 0
-        while x ==0:
-            i = 0
-            if p1_bots[i].r == b.r and p1_bots[i].c == b.c:
-                p1_bots.pop(i)
-                x = 1
-            i += 1
+        else:
+            b.alive = False
        
     
     for r in range(1,8):
         for c in range(1,7):
-            Label(master, image = img[board[r][c]]).grid(row=r,column=c)
+            labels[r][c]['image'] = img[board[r][c]]
+    
+    
     if finish:
         master.after(1000, update)
            
     
             
-       
+def destroy():
+    master.destroy()
 
-    
+def restart():
+    global p1s
+    global p2s
+    p1s = 0
+    p2s = 0
+    play_again()
 
-
-  
-#making the board
-for r in range(1,8):
-    for c in range(8):
-        if c == 0 or c == 7:
-            Label(master, image = img[8]).grid(row = r, column = c)
-        else:
-            Label(master, image = img[1]).grid(row = r, column = c)
-
-Label(master, image = img[0]).grid(row = 4, column = 0)
-Label(master, image = img[0]).grid(row = 4, column = 7)
-Label(master, image = img[5]).grid(row = 9, column = 0, pady=10)
-Label(master, image = img[5]).grid(row = 9, column = 5, pady=10)
-Label(master, image = img[3]).grid(row = 9, column = 1, pady=10)
-Label(master, image = img[3]).grid(row = 9, column = 6, pady=10)
-Label(master, image = img[4]).grid(row = 9, column = 2, pady=10)
-Label(master, image = img[4]).grid(row = 9, column = 7, pady=10)
+Button(master, text='Exit',command=destroy).grid(row= 0, column = 7)
+Button(master, text="Restart", command=restart).grid(row=0, column= 6)
 
 
 master.bind('<Up>', row_up)
--- bots.py
+++ bots.py
@@ -1,31 +1,56 @@
 class Bot:
-    #r =2, p= 3, s=4
-    def __init__(self, rps, r, c):
+    #player 1 r =2, p=3, s=4
+    #player 2 r = 12, p = 13, s = 14
+    def __init__(self, rps, r, c, board):
         self.rps = rps
         self.r = r
         self.c = c
+        self.board = board
+        self.alive = True
+    
+    def destroy(self):
+        self.board[self.r][self.c] = 1
+        self.alive = False
+
     #return 1 if win, 0 if tie, -1 if lose
     def comp(self, other):
+        if self.rps > 4:
+            rps1 = self.rps - 10
+        else:
+            rps1 = self.rps
+        if other.rps > 4:
+            rps2 = other.rps - 10
+        else:
+            rps2 = other.rps
         #self is rock and tie or lose
-        if self.rps == 2:
-            if other.rps == 2:
-                return 0
-            elif other.rps == 3:
-                return -1
+        if rps1 == 2:
+            if rps2 == 2:
+                self.destroy()
+                other.destroy()
+                return
+            elif rps2 == 3:
+                self.destroy()
+                return
         #self is paper and tie or lose
-        if self.rps == 3:
-            if other.rps == 3:
-                return 0
-            elif other.rps == 4:
-                return -1
+        if rps1 == 3:
+            if rps2 == 3:
+                self.destroy()
+                other.destroy()
+                return
+            elif rps2 == 4:
+                self.destroy()
+                return
         #self is sci and tie or lose
-        if self.rps == 4:
-            if other.rps == 4:
-                return 0
-            elif other.rps == 2:
-                return -1
+        if rps1 == 4:
+            if rps2 == 4:
+                self.destroy()
+                other.destroy()
+                return
+            elif rps2 == 2:
+                self.destroy()
+                return
         #self wins
-        return 1
+        other.destroy()
 
     def __str__(self):
         s = str(self.rps) + ', ' + str(self.r) + ', ' +  str(self.c)
